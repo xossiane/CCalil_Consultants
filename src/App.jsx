2f138d6362b52b7306061ca9d50d7fc5ea5e7794
xossiane <anejosi.lima@gmail.com>
2023-03-05 13:09:40 -0300

    Fix: Removing MainCard from site

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 
 import HomePage from './components/pages/Home'
-import MainCard from './components/pages/MainCard'
-import { Routes, Route } from "react-router-dom";
+
+
 
 
 function App() {
 
   return (
-    <Routes>
+   <>
       
-      <Route path="/" element={<HomePage />}></Route>
-      <Route path="/maincard" element={<MainCard/>}></Route>
+      <HomePage />
       
+      </> 
 
 
-    </Routes>
+    
     
   )
 }
